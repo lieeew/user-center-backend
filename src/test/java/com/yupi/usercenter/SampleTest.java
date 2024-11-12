30978bb16156b4ea9b6bfd4b2e8adcc442d0f981
leikooo <108205879+lieeew@users.noreply.github.com>
2024-11-12 20:10:36 +0800

    update

--- src/test/java/com/yupi/usercenter/SampleTest.java
+++ src/test/java/com/yupi/usercenter/SampleTest.java
@@ -4,15 +4,12 @@ import com.baomidou.mybatisplus.core.toolkit.Assert;
 import com.yupi.usercenter.mapper.UserMapper;
 import com.yupi.usercenter.model.User;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
 import java.util.List;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
 public class SampleTest {
 
     @Resource
